179deba242c94364c4d1d97336d49c1acb36c31b
adryanfrancis1 <adriano.amedeiros@hotmail.com>
2022-04-16 20:31:21 -0300

    as excessoes de notblank e length foram tratadas.

--- src/main/java/com/estoque/lojadeinstrumento/LojadeinstrumentoApplication.java
+++ src/main/java/com/estoque/lojadeinstrumento/LojadeinstrumentoApplication.java
@@ -2,7 +2,13 @@ package com.estoque.lojadeinstrumento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EntityScan(basePackages = {"com.estoque.lojadeinstrumento.entidades"})
+@EnableJpaRepositories(basePackages = {"com.estoque.lojadeinstrumento.repositorio"})
+@ComponentScan(basePackages = {"com.estoque.lojadeinstrumento.servico", "com.estoque.lojadeinstrumento.controlador","com.estoque.lojadeinstrumento.excessao"})
 @SpringBootApplication
 public class LojadeinstrumentoApplication {
 
